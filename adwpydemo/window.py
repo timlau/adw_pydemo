8d635d54e629b34598a56c8e541f6a05770bf3fd
Tim Lauridsen <tla@rasmil.dk>
2021-10-25 08:45:35 +0200

    Add at Adw.Flap with a stack & stacksidebar

--- adwpydemo/window.py
+++ adwpydemo/window.py
@@ -2,25 +2,81 @@ from gi.repository import Gtk, Gio, Adw, GLib
 
 from adwpydemo.const import Constants
 
+
+def get_label(text):
+    lbl = Gtk.Label()
+    lbl.set_text(text)
+    lbl.props.halign = Gtk.Align.CENTER
+    lbl.props.valign = Gtk.Align.CENTER
+    lbl.props.hexpand = True
+    lbl.props.vexpand = True
+    return lbl
+
 @Gtk.Template(resource_path=f'{Constants.PATHID}/ui/mainwindow.ui')
 class MainWindow(Adw.ApplicationWindow):
-    __gtype_name__ = 'MainWindow'    
-    
+    __gtype_name__ = 'MainWindow'
+
     main_content = Gtk.Template.Child()
     app_menu = Gtk.Template.Child()
-    
+
     def __init__(self, **kwargs):
-         Adw.ApplicationWindow.__init__(self, **kwargs)
-         # Setup Headerbar
-         self.headerbar = Gtk.HeaderBar()
-         self.btn_menu = Gtk.MenuButton()
-         self.btn_menu.props.icon_name = 'open-menu-symbolic'
-         self.btn_menu.set_menu_model(self.app_menu)
-         title = Gtk.Label()
-         title.set_text('Libadwaita Python Demo')
-         self.headerbar.set_title_widget(title)
-         self.headerbar.pack_end(self.btn_menu)
-         self.main_content.append(self.headerbar)
+        Adw.ApplicationWindow.__init__(self, **kwargs)
+        # Setup Headerbar
+        self.headerbar = Adw.HeaderBar()
+        self.btn_menu = Gtk.MenuButton()
+        self.btn_menu.props.icon_name = 'open-menu-symbolic'
+        self.btn_menu.set_menu_model(self.app_menu)
+        # Flap toggle
+        self.btn_flap = Gtk.ToggleButton()
+        self.btn_flap.props.icon_name = 'go-next-symbolic'
+        self.btn_flap.set_active(True)
+        self.btn_flap.connect('toggled', self.on_flap_toggled)
+        self.headerbar.pack_start(self.btn_flap)
+        # setup menu actions
+        self.create_action('new', self.menu_handler)
+        self.create_action('about', self.menu_handler)
+        self.create_action('quit', self.menu_handler)
+        self.headerbar.pack_end(self.btn_menu)
+        self.main_content.append(self.headerbar)
+        self.flap = Adw.Flap()
+        self.stack = Gtk.Stack()
+        self.page1 = self.add_page('page1', 'Page 1')
+        self.page2 = self.add_page('page2', 'Page 2')
+        self.page3 = self.add_page('page3', 'Page 3')
+        self.page4 = self.add_page('page4', 'Page 4')
+        self.stack_switch = Gtk.StackSidebar()
+        self.stack_switch.set_stack(self.stack)
+        self.flap.set_content(self.stack)
+        self.flap.set_separator(Gtk.Separator())
+        self.flap.set_flap(self.stack_switch)
+        self.main_content.append(self.flap)
+        # self.btn_start = Gtk.Button()
+        # self.btn_start.set_label("Hallo World, This is the Start Button")
+        # self.btn_start.props.halign = Gtk.Align.START
+        # self.btn_start.props.valign = Gtk.Align.START
+        # self.btn_start.props.vexpand = True
+        # self.btn_start.props.hexpand = True
+
+        # self.leaflet.append(self.btn_start)
+        # self.btn_end = Gtk.Button()
+        # self.btn_end.set_label("Hallo World, This is the End Button")
+        # self.btn_end.props.halign = Gtk.Align.END
+        # self.btn_end.props.valign = Gtk.Align.END
+        # self.btn_end.props.vexpand = True
+        # self.btn_end.props.hexpand = True
+        # self.leaflet.append(self.btn_end)
+
+
+    def add_page(self, name, title):
+        box = Gtk.Box()
+        lbl = get_label(title)
+        box.append(lbl)
+        page = self.stack.add_named(box, name)
+        page.set_title(title)
+        return page
+        
+    def on_flap_toggled(self, widget):
+        self.flap.set_reveal_flap(not self.flap.get_reveal_flap())
 
     def on_button_clicked(self, widget):
         label = widget.get_label()
@@ -61,4 +117,3 @@ class MainWindow(Adw.ApplicationWindow):
         action = Gio.SimpleAction.new(name, None)
         action.connect("activate", callback)
         self.add_action(action)
-
